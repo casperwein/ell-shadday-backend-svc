470d937f5af468f61f54f807f3d48f512edd4711
casperWein <19201040.casperweinberger@gmail.com>
2023-05-19 00:55:52 +0700

    enhance-db-bahanbaku-bahan-master-services-bahan-bahan-baku-2

--- controller/bahanbaku/bahanbakuController.js
+++ controller/bahanbaku/bahanbakuController.js
@@ -17,14 +17,14 @@ const GetAllBahanBaku = async(req, res) => {
 
 
 const AddBahanBaku = async(req, res) => {
-    // const gambar = req.file.path
-    const gambar = "\gambar"
-
+    const gambar = req.file.path
     let {suplierId, warna, kodebahan, bahan_bahu_desc, tanggal_terima, tempat_penyimpanan, roll_ball_quantity,
         kg_yard_meter_quantity, is_return, roll_ball_return, yard_kg_meter_return, penerima, kurir, note, faktur, 
         nama, list, list_return, satuan, ukuran, kategori} = req.body
 
     const image = gambar.replace(/\\/g, '/'); // ganti nama gambar
+
+    is_return != "Y" ? (is_return = 'N', roll_ball_return = 0, yard_kg_meter_return = 0, list_return = 0) : (roll_ball_return, yard_kg_meter_return, list_return)
 
     await Bahan.findOne({where: {kodebahan: kodebahan}}).then(bahan => {
         if(!bahan){
@@ -43,7 +43,7 @@ const AddBahanBaku = async(req, res) => {
     await Baku.create({
         suplierId, warna, kodebahan, bahan_bahu_desc, tanggal_terima, tempat_penyimpanan, roll_ball_quantity,
         kg_yard_meter_quantity, is_return, roll_ball_return, yard_kg_meter_return, penerima, kurir, note, faktur, 
-        list, list_return, picture: image
+        list, list_return, gambar: image
     }).then(data_baku => {
         Bahan.findOne({where: {kodebahan: kodebahan}}).then(bahan => {
             const listBahan = list.split(",").map(ls => parseFloat(ls))
@@ -51,7 +51,7 @@ const AddBahanBaku = async(req, res) => {
             const total = listBahan.reduce((acc, sum) => acc + sum, 0)
             
             // save data to bahan
-            bahan.jumlah_unit = bahan.jumlah_unit + jumlah_unit
+            bahan.roll_ball_quantity = bahan.roll_ball_quantity + jumlah_unit
             bahan.yard_kg_clean = bahan.yard_kg_clean + total
             bahan.total_yard_kg = bahan.total_yard_kg + total
             bahan.save()
